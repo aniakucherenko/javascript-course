53775a61481daaf7847a6a64524bd529c3aa2361
aniakucherenko <ania.kucherenko@gmail.com>
2022-11-14 23:57:12 +0200

    update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,18 +1,42 @@
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-const bmiMark = massMark / heightMark ** 2;
-const bmiJohn = massJohn / heightJohn ** 2;
-const markHigherBMI = bmiMark > bmiJohn;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// const bmiMark = massMark / heightMark ** 2;
+// const bmiJohn = massJohn / heightJohn ** 2;
+// const markHigherBMI = bmiMark > bmiJohn;
 
-console.log(bmiMark)
-console.log(bmiJohn)
-console.log(markHigherBMI)
+// console.log(bmiMark)
+// console.log(bmiJohn)
+// console.log(markHigherBMI)
 
-if (bmiMark > bmiJohn) {
-console.log(`Mark has highter BMI, it is ${bmiMark}`);
-} else {
-    console.log(`John has highter BMI, it is ${bmiJohn}`);
-}
+// if (bmiMark > bmiJohn) {
+// console.log(`Mark has highter BMI, it is ${bmiMark}`);
+// } else {
+//     console.log(`John has highter BMI, it is ${bmiJohn}`);
+// }
  
+
+// let Dscore1 = 96
+// let Dscore2 = 108
+// let Dscore3 = 89
+// let Daverage = (Dscore1 + Dscore2 + Dscore3) / 3
+// let Kscore1 = 88
+// let Kscore2 = 91
+// let Kscore3 = 110
+// const minScore = 50
+// let Kaverage = (Kscore1 + Kscore2 + Kscore3) / 3
+
+// console.log(Kaverage, Daverage);
+
+// if (Kaverage > Daverage && Kaverage >= minScore) {
+//     console.log("The Winner is Kaverage");
+//  } else if 
+//    (Daverage > Kaverage && Daverage > minScore) {
+//     console.log("The Winner is Daverage");
+//  } else if
+//     (Kaverage === Daverage && Kaverage >minScore && Daverage >minScore) {
+//     console.log("Both are winners!");
+//  }
+
+
