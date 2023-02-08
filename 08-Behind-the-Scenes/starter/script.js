2a9cc333cf6913cd0c8090969aa28706906152f4
aniakucherenko <ania.kucherenko@gmail.com>
2023-02-08 19:34:03 +0100

    update

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,9 +1,120 @@
 'use strict';
-function calcAge(birthYear) {
-  const age = 2022 - birthYear;
-  return age;
-}
 
-const firstName = 'Anna';
+// function calcAge(birthYear) {
+//   const age = 2022 - birthYear;
+//   console.log(firstName);
 
+//   function printAge() {
+//     const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         console.log(a + b);
+//       }
+//       add(2, 3);
+//     }
+//   }
+
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Anna';
+// calcAge(1987);
+// // console.log(age);
+
+// console.log('!!!!!!!!!!!!!!!');
+
+// // console.log(me);
+// // // console.log(job);
+// // // console.log(year);
+
+// // var me = 'anna';
+// // let job = 'developer';
+// // const year = 1987;
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+// console.log(numProducts);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2022 - birthYear);
+  console.log(this);
+};
 calcAge(1987);
+
+const calcArrow = birthYear => {
+  console.log(2022 - birthYear);
+  console.log(this);
+};
+calcArrow(1987);
+
+// const anna = {
+//   year: 1987,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2022 - this.year);
+//   },
+// };
+// anna.calcAge();
+
+// const matilda = {
+//   year: 2007,
+// };
+
+// matilda.calcAge = anna.calcAge;
+// matilda.calcAge();
+
+// const f = anna.calcAge;
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,82 +1,82 @@
 'use strict';
 
-// Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// // Data needed for a later exercise
+// // const flights =
+// //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// // Data needed for first part of the section
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// // // Data needed for first part of the section
+// // const restaurant = {
+// //   name: 'Classico Italiano',
+// //   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+// //   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+// //   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+// //   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
+// //   openingHours: {
+// //     thu: {
+// //       open: 12,
+// //       close: 22,
+// //     },
+// //     fri: {
+// //       open: 11,
+// //       close: 23,
+// //     },
+// //     sat: {
+// //       open: 0, // Open 24 hours
+// //       close: 24,
+// //     },
+// //   },
+// // };
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
 //   },
 // };
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// for (const [i, player] of game.scored.entries())
+//   console.log(`Goal ${i + 1}: ${player}`);
 
-for (const [i, player] of game.scored.entries())
-  console.log(`Goal ${i + 1}: ${player}`);
-
-const odds = Object.values(game.odds);
-let average = 0;
-for (const odd of odds) average += odd;
-average /= odds.length;
-console.log(average);
+// const odds = Object.values(game.odds);
+// let average = 0;
+// for (const odd of odds) average += odd;
+// average /= odds.length;
+// console.log(average);
 
 // const name = Object.values();
 
@@ -113,123 +113,261 @@ console.log(average);
 //   console.log(`${item[0] + 1}: ${item[1]}`);
 // }
 
-const gameEvents = new Map([
-  [17, 'GOAL'],
-  [36, 'Substitution'],
-  [47, 'GOAL'],
-  [61, 'Substitution'],
-  [64, 'Yellow card'],
-  [69, 'Red card'],
-  [70, 'Substitution'],
-  [72, 'Substitution'],
-  [76, 'GOAL'],
-  [80, 'GOAL'],
-  [92, 'Yellow card'],
-]);
+// const gameEvents = new Map([
+//   [17, 'GOAL'],
+//   [36, 'Substitution'],
+//   [47, 'GOAL'],
+//   [61, 'Substitution'],
+//   [64, 'Yellow card'],
+//   [69, 'Red card'],
+//   [70, 'Substitution'],
+//   [72, 'Substitution'],
+//   [76, 'GOAL'],
+//   [80, 'GOAL'],
+//   [92, 'Yellow card'],
+// ]);
 
-const events = [...new Set(gameEvents.values())];
+// const events = [...new Set(gameEvents.values())];
 
-console.log(gameEvents);
-console.log(events);
-gameEvents.delete(64);
+// console.log(gameEvents);
+// console.log(events);
+// gameEvents.delete(64);
 
-const averages = 90 / gameEvents.size;
-console.log(`The event happened on average ${averages} minutes`);
+// const averages = 90 / gameEvents.size;
+// console.log(`The event happened on average ${averages} minutes`);
 
-for (const [time, event] of gameEvents) {
-  if (time <= 45) {
-    console.log(`First half! ${time}: ${event}`);
-  } else {
-    console.log(`Second half! ${time}: ${event}`);
-  }
-}
+// for (const [time, event] of gameEvents) {
+//   if (time <= 45) {
+//     console.log(`First half! ${time}: ${event}`);
+//   } else {
+//     console.log(`Second half! ${time}: ${event}`);
+//   }
+// }
 
-const airline = 'TAP Air Portugal';
-const plane = 'A320';
+// const airline = 'TAP Air Portugal';
+// const plane = 'A320';
 
-const checkMiddleSeat = function (seat) {
-  const s = seat.slice(-1);
-  if (s === 'B' || s === 'E') console.log('You got the middle place!');
-  else console.log('You are lucky!');
+// const checkMiddleSeat = function (seat) {
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') console.log('You got the middle place!');
+//   else console.log('You are lucky!');
+// };
+
+// checkMiddleSeat('75C');
+// checkMiddleSeat('32B');
+
+// const passenger = 'joNaS';
+// const passengerLowerCase = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLowerCase[0].toUpperCase() + passengerLowerCase.slice(1);
+
+// console.log(passengerCorrect);
+
+// const capitalizeName = function (name) {
+//   const arr = name.split(' ');
+//   const capitalizedWords = [];
+
+//   for (const word of arr) {
+//     // capitalizedWords.push(word[0].toUpperCase() + word.slice(1)).join;
+//     capitalizedWords.push(word.replace(word[0], word[0].toUpperCase()));
+//   }
+
+//   console.log(capitalizedWords.join(' '));
+// };
+
+// capitalizeName('jessica ann smith davis');
+
+// const maskCreditCard = function (number) {
+//   const str = number + '';
+//   const cut = str.slice(-4);
+//   console.log(cut.padStart(str.length, 'X'));
+// };
+
+// maskCreditCard(4149090934569387);
+
+// const planesInLine = function (number) {
+//   console.log(`There are ${number} planes in line ${'✈️'.repeat(number)}`);
+// };
+
+// planesInLine(5);
+// planesInLine(7);
+
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const splitText = text.split('\n');
+//   // console.log(splitText);
+//   for (const [i, word] of splitText.entries()) {
+//     const [first, second] = word.toLowerCase().trim().split('_');
+
+//     const newString = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(`${newString.padEnd(20)}${'✅'.repeat(i + 1)}`);
+
+//     // const newText = `${first} + ${second.replace(second[0], second[0].toUpperCase())
+//     // ).padEnd(20, ' '}✅`;
+//     // console.log(output);
+//   }
+// });
+
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// const array = flights.split('+');
+// console.log(array);
+
+// for (const flight of array) {
+//   const [type, from, to, time] = flight.split(';');
+
+//   const output = `${type.startsWith('_Delayed') ? '⏺' : ''}${type
+//     .split('_')
+//     .join(' ')} from ${from.slice(0, 3).toUpperCase()} to ${to
+//     .slice(0, 3)
+//     .toUpperCase()} (${time.split(':').join('h')}) `;
+
+//   console.log(output.padStart(50));
+// }
+
+const restaurant = {
+  nameA: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
-checkMiddleSeat('75C');
-checkMiddleSeat('32B');
-
-const passenger = 'joNaS';
-const passengerLowerCase = passenger.toLowerCase();
-const passengerCorrect =
-  passengerLowerCase[0].toUpperCase() + passengerLowerCase.slice(1);
-
-console.log(passengerCorrect);
-
-const capitalizeName = function (name) {
-  const arr = name.split(' ');
-  const capitalizedWords = [];
-
-  for (const word of arr) {
-    // capitalizedWords.push(word[0].toUpperCase() + word.slice(1)).join;
-    capitalizedWords.push(word.replace(word[0], word[0].toUpperCase()));
-  }
-
-  console.log(capitalizedWords.join(' '));
-};
-
-capitalizeName('jessica ann smith davis');
-
-const maskCreditCard = function (number) {
-  const str = number + '';
-  const cut = str.slice(-4);
-  console.log(cut.padStart(str.length, 'X'));
-};
-
-maskCreditCard(4149090934569387);
-
-const planesInLine = function (number) {
-  console.log(`There are ${number} planes in line ${'✈️'.repeat(number)}`);
-};
-
-planesInLine(5);
-planesInLine(7);
-
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
-
-const button = document.querySelector('button');
-
-button.addEventListener('click', function () {
-  const text = document.querySelector('textarea').value;
-  const splitText = text.split('\n');
-  // console.log(splitText);
-  for (const [i, word] of splitText.entries()) {
-    const [first, second] = word.toLowerCase().trim().split('_');
-
-    const newString = `${first}${second.replace(
-      second[0],
-      second[0].toUpperCase()
-    )}`;
-    console.log(`${newString.padEnd(20)}${'✅'.repeat(i + 1)}`);
-
-    // const newText = `${first} + ${second.replace(second[0], second[0].toUpperCase())
-    // ).padEnd(20, ' '}✅`;
-    // console.log(output);
-  }
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del sole',
+  mainIndex: 2,
+  starterIndex: 2,
 });
 
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const { nameA, openingHours, categories } = restaurant;
+console.log(nameA, openingHours, categories);
 
-const array = flights.split('+');
-console.log(array);
+const {
+  nameA: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
 
-for (const flight of array) {
-  const [type, from, to, time] = flight.split(';');
+console.log(restaurantName, hours, tags);
 
-  const output = `${type.startsWith('_Delayed') ? '⏺' : ''}${type
-    .split('_')
-    .join(' ')} from ${from.slice(0, 3).toUpperCase()} to ${to
-    .slice(0, 3)
-    .toUpperCase()} (${time.split(':').join('h')}) `;
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
 
-  console.log(output.padStart(50));
-}
+// let o = 111;
+// let e = 999;
+// const obj = { o: 23, e: 7, c: 14 };
+// ({ o, e } = obj);
+// console.log(o, e);
+
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+let [first, , second] = restaurant.categories;
+console.log(first, second);
+
+[first, second] = [second, first];
+console.log(first, second);
+console.log(restaurant.categories);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log('starter: ', starter, ', mainCourse: ', mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Sushi'];
+console.log(newMenu);
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+const menu2 = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu2);
+
+const str = 'Jonas';
+const letters = [...str, '', 'S.'];
+console.log(letters);
+console.log(...letters);
+console.log(...str);
+
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3'),
+// ];
+
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+const newRestaurant = {
+  foundedIn: 1998,
+  ...restaurant,
+  founder: 'Gjuseppe Vaviolli',
+};
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.nameA = 'Ristorante Roma';
+console.log(restaurantCopy.nameA);
+console.log(restaurant.nameA);
