c301b3db66af75a5f86cefc567629cd877ab03a3
aniakucherenko <ania.kucherenko@gmail.com>
2023-02-22 11:07:35 +0100

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -725,3 +725,18 @@ rest
 
 console.log(rest.get('name'));
 console.log(rest.get(true));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+console.log(rest);
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest.size);
+console.log(rest.get(arr));
