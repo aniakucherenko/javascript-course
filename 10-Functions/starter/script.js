1ed972286203bb021177b0b01839858aed352197
aniakucherenko <ania.kucherenko@gmail.com>
2023-02-28 22:31:52 +0100

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -168,45 +168,152 @@
 //     header.style.color = 'blue';
 //   });
 // })();
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  const booking = { flightNum, numPassengers, price };
-  console.log(booking);
-  bookings.push(booking);
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   const booking = { flightNum, numPassengers, price };
+//   // console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH234', 8);
+// createBooking('LN345', undefined, 1000);
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Smith',
+//   passport: 23459889,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH7898';
+//   passenger.name = 'Mr.' + passenger.name;
+//   if (passenger.passport === 23459889) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000);
+// };
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH234', 8);
-createBooking('LN345', undefined, 1000);
-
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Smith',
-  passport: 23459889,
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
 };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH7898';
-  passenger.name = 'Mr.' + passenger.name;
-  if (passenger.passport === 23459889) {
-    alert('Check in');
-  } else {
-    alert('Wrong passport');
-  }
-};
+transformer('Javascript is the best', upperFirstWord);
+transformer('Javascript is the best', oneWord);
 
-checkIn(flight, jonas);
-console.log(flight);
-console.log(jonas);
-
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000);
+const high5 = function () {
+  console.log('🐤');
 };
-newPassport(jonas);
-checkIn(flight, jonas);
+document.body.addEventListener('click', high5);
+['Jonas', 'Marta', 'Kate'].forEach(high5);
+
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+// console.log('hello');
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  //   book: function()
+  //  {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode} ${flightNum}`, name });
+  },
+};
+console.log(lufthansa);
+
+lufthansa.book(239, 'Jonas Smidtman');
+lufthansa.book(625, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+const book = lufthansa.book;
+
+// book(23, 'Sarah Williams');
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+book.call(swiss, 583, 'Mary Cooper');
+
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtman');
+bookEW23('Marta Cooper');
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+lufthansa.buyPlane();
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// console.log('hello');
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0, 23);
+
+console.log(addVAT(100));
+console.log(addVAT(23));
