24c01d653f6dd4c1c893ad4cbc6d347de41e1b75
aniakucherenko <ania.kucherenko@gmail.com>
2023-03-03 21:51:16 +0100

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -221,6 +221,29 @@ const checkDogs = function (dogsJulia, dogsKate) {
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(function (age) {
+    if (age <= 2) {
+      return 2 * age;
+    } else if (age > 2) {
+      return 16 + age * 4;
+    }
+  });
+  const filteredDogs = humanAges.filter(function (age) {
+    return age >= 18;
+  });
+  const sum = filteredDogs.reduce(function (acc, age) {
+    return acc + age;
+  }, 0);
+  const average = sum / filteredDogs.length;
+  console.log(sum);
+  console.log(average);
+  console.log(humanAges);
+  console.log(filteredDogs);
+};
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+
+console.log('!!!!!!!!!');
 const eurToUsd = 1.1;
 
 const movementsUSD = movements.map(mov => mov * eurToUsd);
@@ -239,3 +262,5 @@ const movementsDescriptions = movements.map(
     )}`
 );
 console.log(movementsDescriptions);
+
+movements.filter(function (mov) {});
