534baf943377398c7a8373cb105e7f6da1d17d8c
aniakucherenko <ania.kucherenko@gmail.com>
2023-03-07 06:42:05 +0100

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -264,3 +264,9 @@ const movementsDescriptions = movements.map(
 console.log(movementsDescriptions);
 
 movements.filter(function (mov) {});
+
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
