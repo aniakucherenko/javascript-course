5d6a4a0bab89d02b01aa3e972ce3724cff6a6057
aniakucherenko <ania.kucherenko@gmail.com>
2023-03-30 16:21:25 +0200

    Update script.js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -74,9 +74,33 @@ btnScrollTo.addEventListener('click', function (e) {
 // });
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
-  console.log(e.target);
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
 });
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabscontent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabscontent.forEach(c => c.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 // console.log(document.documentElement);
 // console.log(document.head);
 // console.log(document.body);
@@ -132,7 +156,7 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 // logo.classList.toggle('c');
 // logo.classList.contains('c');
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
 // const alertH1 = function (e) {
 //   alert('Great! You are reading the heading :D');
@@ -160,3 +184,28 @@ const randomColor = () =>
 //   this.style.backgroundColor = randomColor();
 //   console.log('NAV', e.target, e.currentTarget);
 // });
+
+const h1 = document.querySelector('h1');
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
