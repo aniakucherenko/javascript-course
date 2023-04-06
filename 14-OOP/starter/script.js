1e3a9b230a7a07a85adc4187675615011a90729a
aniakucherenko <ania.kucherenko@gmail.com>
2023-04-06 12:43:46 +0200

    Update script.js

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Never use methods inside of construction
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const anna = new Person('Anna', 1987);
+console.log(anna);
+
+// New {} is created
+// func is called, this = {}
+// {} linked to prototype
+// func automatically return {}
+
+const matilda = new Person('Matilda', 1980);
+const jack = new Person('Jack', 1995);
+console.log(matilda, jack);
+
+console.log(anna instanceof Person);
+
+//Prototypes
