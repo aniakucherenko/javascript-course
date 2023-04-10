0946f17d219abe076a283090c92cbd4140539db1
aniakucherenko <ania.kucherenko@gmail.com>
2023-04-10 09:00:10 +0200

    Update script.js

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -267,3 +267,70 @@ console.log(tesla);
 // ford.brake();
 // ford.brake();
 // console.log(ford.speedUS)
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    // console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    // console.log(`Hey, ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static hey() {
+    console.log('Hey there!!!=)');
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(`My name is ${this.fullName} and i study ${this.course}`);
+  }
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student i fell more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2021, 'Computer Science');
+martha.introduce();
+martha.calcAge();
+
+const PersonProto = {
+  calcAge() {
+    // console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
