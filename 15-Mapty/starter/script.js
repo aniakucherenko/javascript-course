a8088d6880214bf97e598b041f28f7faa8faa58c
aniakucherenko <ania.kucherenko@gmail.com>
2023-05-07 22:40:23 +0200

    Update script.js

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -281,7 +281,11 @@ class App {
       this._renderWorkout(work);
     });
   }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
+  }
 }
 
 const app = new App();
-// app._getPosition();
